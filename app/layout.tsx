3b394f11fda6c396020399534f683c91c28bf494
jdy8739 <jdy8739@naver.com>
2024-11-01 20:44:39 +0900

    feat: list page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './globals.css';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-slate-700">{children}</body>
     </html>
   );
 }
